f83d73dfddd06176fad5e3ec2fb705933c55014b
Rafid46 <mostafarafid45@gmail.com>
2024-02-08 19:40:08 +0600

    folder created

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
+const port = process.env.PORT || 5010;
+app.use(express.json());
+
+app.use(cors());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.j1gssm8.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const taskCollection = client.db("todoApp").collection("tasks");
+    // add task
+    app.post("/todo/tasks", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("todo server is running");
+});
+
+app.listen(port, () => {
+  console.log(`todo server in running port:${port}`);
+});
---
+++ src/Components/Shared/Navbar.jsx
@@ -0,0 +1,88 @@
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  const links = (
+    <div>
+      <div className="">
+        <nav className="flex flex-col md:flex-row lg:flex-row items-center justify-between font-poppins">
+          <li className="text-xl mr-10 font-semibold text-[#7F27FF]">
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-gray-800" : ""
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="text-xl mr-10 font-semibold text-[#7F27FF]">
+            <NavLink
+              to="dashboard"
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-gray-100" : ""
+              }
+            >
+              Dashboard
+            </NavLink>
+          </li>
+          <li className="text-xl font-semibold text-[#7F27FF]">
+            <NavLink
+              to="/login"
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-gray-800" : ""
+              }
+            >
+              Login
+            </NavLink>
+          </li>
+        </nav>
+      </div>
+    </div>
+  );
+  return (
+    <div>
+      <div>
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                {links}
+              </ul>
+            </div>
+            {/* <div className="w-[100px]">
+       
+            </div> */}
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal py-1">{links}</ul>
+          </div>
+          <div className="navbar-end text-3xl"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
